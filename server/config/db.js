7a69bba25e3818c55481a47a4ae2d531d7c907ba
David E. Bato bato <149306718+DavidBatoDev@users.noreply.github.com>
2024-04-12 22:06:02 +0800

    Completed Setup

--- server/config/db.js
+++ server/config/db.js
@@ -1,1 +1,15 @@
 // db here
+const mongoose = require('mongoose');
+
+const DATABASE_URI = process.env.DATABASE_URI 
+
+const connectDb = async () => {
+    try {
+        await mongoose.connect(DATABASE_URI)
+        console.log('MongoDB is connected')
+    } catch (error) {
+        console.log(error)
+    }
+}
+
+module.exports = connectDb;
--- server/server.js
+++ server/server.js
@@ -1,1 +1,15 @@
-console.log("hello world");
+require('dotenv').config()
+const express = require('express')
+const connectDb = require('./config/db')
+
+const app = express()
+
+connectDb()
+
+app.get('/', (req, res) => {
+    res.status(200).json({ message: 'Hello World' })
+})
+
+app.listen(8080, () => {
+    console.log('Server is running on http://localhost:8080')
+})
