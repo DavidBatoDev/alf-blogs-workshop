e36032f16ea14a498c3693b905529ea79447c753
David E. Bato bato <149306718+DavidBatoDev@users.noreply.github.com>
2024-04-16 15:27:46 +0800

    Done pasting All the Blur Effects

--- client/src/components/ArticleSection.jsx
+++ client/src/components/ArticleSection.jsx
@@ -5,6 +5,7 @@ import CardsContainer from "./CardsContainer";
 import Searchbar from "./Searchbar";
 import RegularCard from "./RegularCard";
 import { API_URL } from "../constants";
+import Blur from "./Blur";
 
 const mockData = [
   {_id: 1, title: "AASHIDOASIODASHO ASDSA", date: Date.now(), cover_photo: "/preview.png", content: "dasdasd"},
@@ -57,6 +58,30 @@ const ArticleSection = () => {
 
   return (
     <div id="articleSection">
+       <Blur
+        h={"60%"}
+        w={"45%"}
+        bg={"#7000FF"}
+        x={"0"}
+        y={"25%"}
+        opacity={0.15}
+        blur={"400px"}
+        translate_x={"-50%"}
+        translate_y={"-50%"}
+        border_radius={"100%"}
+      />
+      <Blur
+        h={"50%"}
+        w={"35%"}
+        bg={"#60FFE7"}
+        x={"80%"}
+        y={"80%"}
+        opacity={0.15}
+        blur={"400px"}
+        translate_x={"-50%"}
+        translate_y={"-50%"}
+        border_radius={"100%"}
+      />
       <div className="article-sec-heading-container">
         <p className="article-sec-heading">Learn About Everything Tech!</p>
         <p className="article-sec-subheading">brought to you by AWSCC Department of Software and Web Development</p>
--- client/src/components/CtaSection.jsx
+++ client/src/components/CtaSection.jsx
@@ -1,10 +1,35 @@
 import React from 'react'
 import '../styles/CtaSection.css'
 import Button from './Button'
+import Blur from './Blur'
 
 const CtaSection = () => {
   return (
     <div className='cta-section' id='ctaSection'>
+      <Blur
+        h={"40%"}
+        w={"50%"}
+        bg={"#7000FF"}
+        x={"35%"}
+        y={"52.5%"}
+        opacity={0.32}
+        blur={"200px"}
+        translate_x={"-50%"}
+        translate_y={"-50%"}
+        border_radius={"100%"}
+      />
+      <Blur
+        h={"40%"}
+        w={"50%"}
+        bg={"#FF00C7"}
+        x={"65%"}
+        y={"52.5%"}
+        opacity={0.32}
+        blur={"200px"}
+        translate_x={"-50%"}
+        translate_y={"-50%"}
+        border_radius={"100%"}
+      />
         <h1 className='cta-section-heading'>Hurry up and join our newsletter~</h1>
         <h2 className='cta-section-subheading'>
             Only at Alf Blogs. You can learn more about different topics regarding web whikle also learning about everything cloud!
